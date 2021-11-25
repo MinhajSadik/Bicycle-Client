a68cd55ed7b95179279608421e1882f6dfa17926
MinhajSadik <MinhajoSadik@gmail.com>
2021-11-25 18:03:10 +0600

    [Done] Styling issue solved

--- src/Components/SharedComponents/NavBar/NavBar.js
+++ src/Components/SharedComponents/NavBar/NavBar.js
@@ -10,59 +10,59 @@ import "./Navbar.css";
 import UserAction from "./UserAction/UserAction";
 
 const NavBar = () => {
-    // eslint-disable-next-line no-unused-vars
-    const [user, setUser] = useContext(UserContext);
-    const [toggle, setToggle] = useState("none");
-    const profileOpen = () => {
-        toggle === "none" ? setToggle(" ") : setToggle("none");
-    };
-    const profileClose = () => {
-        setToggle(" ");
-    }
+  // eslint-disable-next-line no-unused-vars
+  const [user, setUser] = useContext(UserContext);
+  const [toggle, setToggle] = useState("none");
+  const profileOpen = () => {
+    toggle === "none" ? setToggle(" ") : setToggle("none");
+  };
+  const profileClose = () => {
+    setToggle(" ");
+  };
 
-    return (
-        <Navbar expand="lg" className="NavBarPart">
-            <Navbar.Brand href="/home" className="logo">
-                <img src={logo} alt="" />
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="ml-auto">
-                    <div className="navMenu">
-                        <Link to="/home">Home</Link>
-                        <Link to="/dashboard">Dashboard</Link>
-                        <Link to="/dashboard/dashboardBookNow">
-                            Services
-						</Link>
-                        {user?.email ? (
-                            <Button
-                                onClick={profileOpen}
-                                className="profileBtn"
-                            >
-                                <img
-                                    src={
-                                        user?.photoURL ||
-                                        profileImage
-                                    }
-                                    alt=""
-                                />
-								Profile
-                                <FontAwesomeIcon
-                                    className="ml-2"
-                                    icon={faSortDown}
-                                />
-                            </Button>
-                        ) : (
-                            <Link to="/login">LogIn</Link>
-                        )}
-                    </div>
-                </Nav>
-            </Navbar.Collapse>
-            <div className="nestedNav" style={{ display: toggle }} onClick={profileClose}>
-                <UserAction />
-            </div>
-        </Navbar>
-    );
+  return (
+    <Navbar expand="lg" className="NavBarPart">
+      <Navbar.Brand href="/home" className="logo">
+        <img src={logo} alt="" />
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto">
+          <div className="navMenu">
+            <Link to="/home">Home</Link>
+          </div>
+          <div className="navMenu">
+            <Link to="/dashboard">Dashboard</Link>
+          </div>
+          <div className="navMenu">
+            <Link to="/dashboard/dashboardBookNow">Services</Link>
+          </div>
+          <div className="navMenu">
+            {user?.email ? (
+              <Button onClick={profileOpen} className="profileBtn">
+                <img
+                  style={{ width: "30px", height: "30px" }}
+                  src={user?.photoURL || profileImage}
+                  alt=""
+                />
+                Profile
+                <FontAwesomeIcon className="ml-2" icon={faSortDown} />
+              </Button>
+            ) : (
+              <Link to="/login">LogIn</Link>
+            )}
+          </div>
+        </Nav>
+      </Navbar.Collapse>
+      <div
+        className="nestedNav"
+        style={{ display: toggle }}
+        onClick={profileClose}
+      >
+        <UserAction />
+      </div>
+    </Navbar>
+  );
 };
 
 export default NavBar;
